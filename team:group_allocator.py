5f15901b398b4e8b93ad26b598b3c06fabe11a63
Tomohiro Samma <51979540+tomosukhumvit@users.noreply.github.com>
2020-03-19 00:00:56 +0900

    Add files via upload

---
+++ team:group_allocator.py
@@ -0,0 +1,32 @@
+import random
+
+players = ["Martin","Craig","Sue",
+           "Claire","Dave","Alice",
+           "Sonakshi","Harry","Jack",
+           "Rose","Lexi","Maria",
+           "Thomas","James","William",
+           "Ada","Grace","Jean",
+           "Marissa","Alan"]
+
+print("Welcome to Team/Player Allocator!")
+while True:
+    random.shuffle(players)
+
+    response = input ("Is it a team or individual sport?\ \nType team or individual: ")
+    if response == "team":
+        team1 = players[:len(players)//2]
+        print("Team 1 captain: " + random.choice(team1))
+        print("Team 1:")
+        for player in team1:
+            print(player)
+        team2 = players[len(players)//2:]
+        print("\nTeam 2 captain: " + random.choice(team2))
+        print("Team 2:")
+        for player in team2:
+            print(player)
+    else:
+        for i in range(0, 20, 2):
+            print(players[i] + " vs " + players[i+1])
+            start = random.randrange(i, i+2)
+            print(players[start] + " starts")
+            
